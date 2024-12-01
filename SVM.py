754db191ce804ea7d26253d05c665d1f4a4e021b
Kamal Chanchal <40321363+Coderixc@users.noreply.github.com>
2024-12-02 00:18:22 +0530

    #2 SVM Model : Working state

---
+++ SVM.py
@@ -0,0 +1,67 @@
+import DataGenerator.OHLCGenerator as Fd
+import pandas as pd
+from sklearn.svm import SVC, SVR
+
+from sklearn.model_selection import train_test_split
+
+#https://chatgpt.com/c/674c89be-9040-8011-b7dc-0aecdee08435
+class SVM():
+    """
+    Support Vector Machines (SVM) are supervised learning models
+    Used - classification and regression problems.
+    Aim - find an optimal hyperplane in a multi-dimensional space
+        that separates the data into classes (classification)
+        or fits a relationship (regression).
+
+    Key Concepts
+    Hyperplane: A decision boundary that separates classes in the feature space.
+    Support Vectors: Data points that are closest to the hyperplane and influence its position.
+    Margin: The distance between the hyperplane and the nearest data points (support vectors).
+    SVM maximizes this margin.
+
+    Distance between Margin and Huperplane : we use Euclidean distance
+
+    """
+
+    def __init__(self,df_OHLC: pd.DataFrame):
+        # Validate df is a Pandas DataFrame
+        if not isinstance(df_OHLC, pd.DataFrame):
+            raise TypeError("Input 'df' must be a pandas DataFrame.")
+
+        self.df= df_OHLC
+
+    def Preprocess(self):
+        """
+        Preprocess the injected dataframe to create features and Target
+
+        Operation :
+        - pct_change = (Current_Value - Prev_value) / Prev_value
+        - Direction : 1 for up  , 0 for down
+        - Trading Indicator : MA(5) and MA(10)
+        """
+        df = self.df.copy(deep= True)  #deep = data(independent) + structure
+
+        #add a 'Return' column as daily return
+        df['Return'] = df['Close'].pct_change()
+
+        #calculate Direction using True(1) , False(0) , Converting Boolean to int using astype(int)
+        df['Direction'] = (df['Return'] > 0).astype(int)
+
+        df['MA5'] = df['Close'].rolling(window=5).mean()
+        df['MA10'] = df['Close'].rolling(window=10).mean()
+        df = df.dropna()
+
+        if len(df) <=0:
+            print("No Data")
+            return
+
+        # Define features and target
+        #synatax : [[]]  = for selecting multiple columns
+        #[] = list type
+        #[ list type ]
+
+        #df['Open'] -- series type
+        #df[['Open']] -- dataframe type
+        X = df[['Open', 'High', 'Low', 'MA5', 'MA10']]  #as dataframe
+        y = df['Direction']  #as series
+        return train_test_split(X, y, test_size=0.2, random_state=42)
